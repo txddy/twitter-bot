f452ac3fef61e3303abb8865311ac2e0d5c9e5b5
txddy <39346681+txddy@users.noreply.github.com>
2018-05-18 12:21:19 -0300

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Oi, tudo bom? tenho um canal no Youtube e gostaria muito que você assistisse algum vídeo meu e quem sabe se inscreve-se, é só pesquisar por txddy ou acessar esse video https://www.youtube.com/watch?v=CG2Za1W35CA Muito obrigado. `;
+  return `Oi, tudo bom? tenho um canal no Youtube e gostaria muito que você assistisse algum vídeo meu e quem sabe se inscreve-se, é só pesquisar por txddy ou acessar esse video youtube.com/txddy?sub_confirmation=1 Muito obrigado. `;
 };
 
 module.exports = AutoDM;
