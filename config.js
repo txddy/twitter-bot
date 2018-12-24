82318efc85eea4a3ccebd03ed2151cdc7413fccf
txddy <39346681+txddy@users.noreply.github.com>
2018-12-24 12:13:31 -0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const twitterApp = {
   api_key: process.env.API_KEY,
-  api_secret_key: process.env.APEI_SECRET_KEY,
+  api_secret_key: process.env.API_SECRET_KEY,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
